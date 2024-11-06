e58f887730a6c846a896ed02da3df114e970393e
John Reyes <johnr1044@gmail.com>
2024-11-05 23:32:26 -0500

    Project 7

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-279a6bf3-bb5a-454b-a79f-420685859e6d.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-eb6b81bd-edea-4e53-9fdc-962826900fd7.containerhub.tripleten-services.com"
 }
